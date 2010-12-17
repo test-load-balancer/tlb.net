531ca569fe4add8c73fe7d3b83dd730b926de24a
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2010-12-17 17:54:25 +0530

    adding logic to send http request

--- TLB.NET/LoadBalancedFileSet.cs
+++ TLB.NET/LoadBalancedFileSet.cs
@@ -1,13 +1,39 @@
 using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
 using NAnt.Core.Types;
 
 namespace TLB.NET
 {
     public class LoadBalancedFileSet : FileSet
     {
+        private const string BalancerUrl = "http://10.4.4.52:3001/balance";
+
         public void GetSuiteFilesFromIncludes()
         {
+            //10.4.4.52
+            var listOfIncludeSuites = "foo/bar\nfoo/baz\n43252435/fadlskkfjdsal\na/b\n";
+            var prunedListOfSuites = PostRequestToBalancer(listOfIncludeSuites);
+        }
 
+        private string PostRequestToBalancer(string listOfTestSuites)
+        {
+            HttpWebRequest webRequest = GetWebRequest(listOfTestSuites);
+            var webResponse = webRequest.GetResponse();
+            StreamReader sr = new StreamReader(webResponse.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        private HttpWebRequest GetWebRequest(string listOfTestSuites)
+        {
+            var webRequest = (HttpWebRequest) WebRequest.Create(BalancerUrl);
+            webRequest.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(listOfTestSuites);
+            var requestStream = webRequest.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+            return webRequest;
         }
     }
 }
